e5d7e6826b76cd0c97ff37f06bb0419c06817866
Tony <tony.hetrick@gmail.com>
2013-01-25 23:16:07 +0600

    Initial commit of integrating Google Groups

--- main/urls_auca.py
+++ main/urls_auca.py
@@ -4,7 +4,7 @@ from django.views.generic.simple import redirect_to
 #---ACUA Custom URL's----
 
 urlpatterns = patterns('',
-    
+	
     # Webinars
     url(r'^(?P<course_prefix>[a-zA-Z0-9_-]+)/(?P<course_suffix>[a-zA-Z0-9_-]+)/webinar/?$', 'courses.webinar.views.view'),
     url(r'^courses/webinar/get_recordings/?', 'courses.webinar.views.get_recordings'),
@@ -12,5 +12,8 @@ urlpatterns = patterns('',
 
     url(r'^courses/webinar/start_meeting/?', 'courses.webinar.actions.start_meeting'),
     url(r'^courses/webinar/join_meeting/?', 'courses.webinar.actions.join_meeting'),
- 
+    
+    # Google groups
+    url(r'^(?P<course_prefix>[a-zA-Z0-9_-]+)/(?P<course_suffix>[a-zA-Z0-9_-]+)/google-groups/?$', 'courses.google_groups.views.view'),
+    url(r'^(?P<course_prefix>[a-zA-Z0-9_-]+)/(?P<course_suffix>[a-zA-Z0-9_-]+)/google-groups/update?$', 'courses.google_groups.actions.update'),
 )
