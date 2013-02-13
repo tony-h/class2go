9b58c22bcf68e93337d424cb356b00788bf3345d
Tony <tony.hetrick@gmail.com>
2013-02-13 12:09:35 +0600

    Corrected local download URLs

--- main/c2g/models.py
+++ main/c2g/models.py
@@ -572,7 +572,7 @@ class File(TimestampMixin, Stageable, Sortable, Deletable, models.Model):
     def dl_link(self):
         filename = self.file.name
         if is_storage_local():
-            url = get_site_url() + self.file.storage.url(filename)
+            url = self.file.storage.url(filename)
         else:
             storecache = get_cache("file_store")
             storecache_key = filename.replace(' ','%20')[-240:]   # memcache no spaces in cache key, char limit
@@ -763,7 +763,7 @@ class CourseCertificate(TimestampMixin, models.Model):
         url = ''
         if default_storage.exists(asset_path):
             if is_storage_local():
-                url = get_site_url() + default_storage.url(asset_path)
+                url = default_storage.url(asset_path)
             else:
                 url = default_storage.url_monkeypatched(asset_path, response_headers={'response-content-disposition': 'attachement'})
         return url
@@ -1145,7 +1145,7 @@ class Video(TimestampMixin, Stageable, Sortable, Deletable, models.Model):
                 return ""
             if is_storage_local():
                 # FileSystemStorage returns a path, not a url
-                loc_raw = get_site_url() + self.file.storage.url(videoname)
+                loc = self.file.storage.url(videoname)
             else:
                 loc = self.file.storage.url_monkeypatched(videoname,
                     response_headers={'response-content-disposition': 'attachment'})
@@ -1165,7 +1165,7 @@ class Video(TimestampMixin, Stageable, Sortable, Deletable, models.Model):
         if is_storage_local():
             # FIXME: doesn't work on local sites yet
             print "DEBUG: Multiple download links don't work on local sites yet, sorry." 
-            return [('large', get_site_url() + mystore.url(myname), self.file.size, '')]
+            return [('large', mystore.url(myname), self.file.size, '')]
         else:
             # XXX: very S3 specific
             urlof = mystore.url_monkeypatched
