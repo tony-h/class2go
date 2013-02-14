752dfbae613eceba4e7cf80068ad42dde1d5123a
Tony <tony.hetrick@gmail.com>
2013-02-14 11:17:37 +0600

    Branding Class2Go for AUCA

--- main/settings.py
+++ main/settings.py
@@ -120,11 +120,11 @@ MEDIA_URL = 'http://localhost/git/' + APP + '-files/uploads/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/VertrigoServ/www/git/' + APP + '-files/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://localhost/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
